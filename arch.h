89206e33b61957dce38aa44fb2b2b0cf6a571352
Christoph Kirsch <ckirsch@gmail.com>
2012-12-31 10:49:11 -0800

    Cleaned up licensing and changed license to Simplified BSD License.

--- arch.h
+++ arch.h
@@ -1,11 +1,8 @@
 /*
- * Copyright (c) 2010-2013, the project authors. All rights reserved.
- * Please see the AUTHORS file for details. Use of this source code
- * is governed by a license that can be found in the LICENSE file.
- *
- * Computational Systems Group, cs.uni-salzburg.at/~ck/group
- * Department of Computer Sciences, cs.uni-salzburg.at
- * University of Salzburg, www.uni-salzburg.at
+ * Copyright (c) 2010, the Short-term Memory Project Authors.
+ * All rights reserved. Please see the AUTHORS file for details.
+ * Use of this source code is governed by a BSD license that
+ * can be found in the LICENSE file.
  */
 
 #ifndef _ARCH_H
