47dcda586fbf34ac8e3a2749493d0fe2a33f78be
Christoph Kirsch <ckirsch@gmail.com>
2012-12-30 18:48:57 -0800

    Attempt at mentioning authors and license in one place. The arch.h file contains a draft header for this purpose.

--- arch.h
+++ arch.h
@@ -1,26 +1,11 @@
 /*
- * Copyright (c) 2010 Martin Aigner, Andreas Haas
- * http://cs.uni-salzburg.at/~maigner
- * http://cs.uni-salzburg.at/~ahaas
+ * Copyright (c) 2010-2013, the project authors. All rights reserved.
+ * Please see the AUTHORS file for details. Use of this source code
+ * is governed by a license that can be found in the LICENSE file.
  *
- * University Salzburg, www.uni-salzburg.at
- * Department of Computer Science, cs.uni-salzburg.at
- */
-
-/*
- *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
- *  (at your option) any later version.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * Computational Systems Group, cs.uni-salzburg.at/~ck/group
+ * Department of Computer Sciences, cs.uni-salzburg.at
+ * University of Salzburg, www.uni-salzburg.at
  */
 
 #ifndef _ARCH_H
