e84089a3990e42434253ab7acaf35482c57e956b
Martin Aigner <martin@maigner.net>
2013-02-28 12:09:25 +0100

    added libscm example

---
+++ examples/prog1.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include "stm.h"
+
+void *pointer2 = NULL;
+
+void use_some_memory() {
+	//this pointer is only used within this scope
+	void *pointer1 = scm_malloc(10);
+	//let pointer1 expire after this round
+	scm_refresh(pointer1, 0);
+	
+	if (pointer2 != NULL) {
+		//memory at pointer2 from previous round
+		//.. do something with pointer2
+	}
+	
+	//create new memory for pointer2
+	pointer2 = scm_malloc(20);
+	//refresh pointer2 to be vaid in the next round
+	scm_refresh(pointer2, 1);
+}
+
+int main(int argc, char** argv) {
+	
+	int i;
+	
+	for (i = 0; i < 10; i++) {
+		//...
+		use_some_memory();
+		scm_tick();
+		//...
+	}
+	
+	return 0;
+}
