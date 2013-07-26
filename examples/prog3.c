ecda8fda4062c8b6db30dec465eb7d065921c59e
Christoph Kirsch <ckirsch@gmail.com>
2013-07-25 18:00:39 -0700

    Added prog3.

--- examples/prog3.c
+++ examples/prog3.c
@@ -30,6 +30,19 @@ int main(int argc, char** argv) {
 	region1 = scm_create_region();
 	const int region2 = scm_create_region();
 
+	const int clock1 = scm_register_clock();
+	const int clock2 = scm_register_clock();
+
+	if((clock2-1) != clock1) {
+		printf("1) Error while creating clocks: %d != %d\n", clock1, clock2);
+		exit(0);
+	}
+
+	if(clock1 < 0 || clock2 < 0) {
+		printf("2) Error while creating clocks\n");
+		exit(0);
+	}
+
 	for (i = 0; i < 10; i++) {
 		//...
 		use_some_memory();
@@ -39,6 +52,7 @@ int main(int argc, char** argv) {
 			for(j=0; j<LOOPRUNS; j++) {
 				void* ptr = scm_malloc_in_region(MEMSIZE2, region2);
 				scm_refresh(ptr, 0);
+				scm_refresh_with_clock(ptr, clock1, 0);
 			}
 			printf("|-------------------------------------------|\n");
 		}
@@ -46,11 +60,19 @@ int main(int argc, char** argv) {
 		//...
 	}
 
-	printf("------------- deallocation: --------------\n");
-	for(i=0; i<60; i++) {
+	printf("------------- second clock tick: --------------\n");
+	for(i = 0; i < 10; i++) {
+		if(i % 2 == 0) {
+			for(i=0; i<LOOPRUNS; i++) {
+				scm_tick_clock(clock1);
+			}
+		}
+	}
+
+	for(i = 0; i < 61; i++) {
 		scm_tick();
 	}
 
-	printf("prog2: success!\n");
+	printf("prog3: success!\n");
 	return 0;
 }
