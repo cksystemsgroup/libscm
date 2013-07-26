f5ce456f613796ccf788d5537e7507e2206a04c0
Christoph Kirsch <ckirsch@gmail.com>
2013-07-25 18:20:41 -0700

    Testing without even creating a second region.

--- examples/prog2.c
+++ examples/prog2.c
@@ -28,7 +28,7 @@ int main(int argc, char** argv) {
 	int i, j;
 
 	region1 = scm_create_region();
-	const int region2 = scm_create_region();
+	//const int region2 = scm_create_region();
 
 	for (i = 0; i < 10; i++) {
 		//...
